1fb55929e2103dfe70ed86b86233d6c832ab05ac
Viswanadh <viswanadh@Admins-MacBook-Pro-3.local>
2025-03-06 11:06:29 +0530

    Updated sequilize config

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,12 +5,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize({
-    database: process.env.DATABASE ?? "task",
-    host: process.env.HOST ?? "localhost",
-    port: Number(process.env.DB_PORT) ?? 3306,
-    dialect: "mysql",
-    username: process.env.USERNAME ?? "root",
-    password: process.env.DB_PASSWORD || "password",
-    models: [UserModel, TaskModel],
-});
+// export const sequelize = new Sequelize({
+//     database: process.env.DATABASE ?? "task",
+//     host: process.env.HOST ?? "localhost",
+//     port: Number(process.env.DB_PORT) ?? 3306,
+//     dialect: "mysql",
+//     username: process.env.USERNAME ?? "root",
+//     password: process.env.DB_PASSWORD || "password",
+//     models: [UserModel, TaskModel],
+// });
+
+export const sequelize = new Sequelize(
+    process.env.DATABASE as string,
+    process.env.USERNAME as string,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.HOST,
+        dialect: "mysql",
+        port: Number(process.env.DB_PORT),
+        models: [UserModel, TaskModel],
+    }
+);
