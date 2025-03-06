2961979695a705b1c1aa0994f763b8a57df67582
Viswanadh Mudunuru <82888548+viswanadh7@users.noreply.github.com>
2025-03-06 16:06:04 +0530

    Update database.ts debug

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,24 +5,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const sequelize = new Sequelize({
-//     database: process.env.DATABASE ?? "task",
-//     host: process.env.HOST ?? "",
-//     port: Number(process.env.DB_PORT) ?? 3306,
-//     dialect: "mysql",
-//     username: process.env.USERNAME ?? "root",
-//     password: process.env.DB_PASSWORD || "password",
-//     models: [UserModel, TaskModel],
-// });
-
-export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-    dialect: "mysql",
-    dialectOptions: {
-        connectTimeout: 60000,
-    },
-    host: process.env.DB_HOST,
+console.log({
+    database: process.env.DATABASE ?? "task",
+    host: process.env.HOST ?? "",
     port: Number(process.env.DB_PORT),
-    define: { timestamps: false },
-    logging: false,
+    dialect: "mysql",
+    username: process.env.USERNAME ?? "root",
+    password: process.env.DB_PASSWORD || "password",
+    models: [UserModel, TaskModel],
+});
+export const sequelize = new Sequelize({
+    database: process.env.DATABASE ?? "task",
+    host: process.env.HOST ?? "",
+    port: Number(process.env.DB_PORT) ?? 3306,
+    dialect: "mysql",
+    username: process.env.USERNAME ?? "root",
+    password: process.env.DB_PASSWORD || "password",
     models: [UserModel, TaskModel],
 });
